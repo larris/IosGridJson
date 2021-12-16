06a5ebf3765d6c596b4cf118c94df191b72777e7
Lazaros Fantidis <lazaros.fantidis@redeye.com>
2021-12-16 21:25:52 +0000

    Added search bar functionality

--- GridApp/ContentView.swift
+++ GridApp/ContentView.swift
@@ -12,16 +12,23 @@ import Kingfisher
 struct ContentView: View {
     
     @ObservedObject var vm = GridviewModel()
+    @State var searchText = ""
+    @State var isSearching = false
+ 
     
     var body: some View {
         NavigationView {
             ScrollView {
+                //Search bar
+                SearchBar(searchText: $searchText, isSearching: $isSearching)
+            
+                //Grid
                 LazyVGrid(columns: [
                     GridItem(.flexible(minimum: 100, maximum: 200),spacing: 12,alignment: .top),
                     GridItem(.flexible(minimum: 100, maximum: 200),spacing: 12,alignment: .top),
                     GridItem(.flexible(minimum: 100, maximum: 200),alignment: .top),
                 ],  spacing: 12,  content: {
-                    ForEach(vm.results, id: \.self){ rs in
+                    ForEach((vm.results).filter({"\($0)".contains(searchText) || searchText.isEmpty}), id: \.self){ rs in
                         GridViewArtists(rs: rs)
                     }
                     
@@ -35,6 +42,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
             ContentView()
+            ContentView().colorScheme(ColorScheme.dark)
         }
     }
 }
@@ -56,5 +64,58 @@ struct GridViewArtists: View {
                 .foregroundColor(.gray)
         }
         .padding(.horizontal)
+    }
+}
+
+struct SearchBar: View {
+    @Binding var searchText: String
+    @Binding var isSearching: Bool
+    
+    var body: some View {
+        HStack {
+            HStack{
+                TextField("Search terms here", text: $searchText)
+                    .padding(.leading,24)
+            }
+            .padding()
+            .background(Color(.systemGray5))
+            .cornerRadius(10)
+            .padding(.horizontal)
+            .onTapGesture {
+                isSearching = true
+            }
+            .overlay {
+                HStack {
+                    Image(systemName: "magnifyingglass")
+                    Spacer()
+                    if isSearching {
+                        Button {
+                            searchText = ""
+                        } label: {
+                            Image(systemName: "xmark.circle.fill")
+                                .padding(.vertical)
+                        }
+                        
+                    }
+                    
+                }.padding(.horizontal,32)
+                    .foregroundColor(.gray)
+            }
+            
+            if isSearching {
+                Button {
+                    isSearching = false
+                    searchText = ""
+                    
+                    //Dismiss keyboard
+                    UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+                } label: {
+                    Text("Cancel")
+                        .padding(.trailing)
+                        .padding(.leading,0)
+                }
+                .transition(.move(edge: .trailing))
+            }
+        }
     }
 }
